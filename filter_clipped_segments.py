97869e6d364d3fe02591fdedcb20a7e91dda301a
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2023-07-03 16:03:39 +0200

    changes

--- filter_clipped_segments.py
+++ filter_clipped_segments.py
@@ -18,16 +18,20 @@ from sklearn.model_selection import train_test_split
  #   return signals_fitered
 
 
-def clipping_filter_normalized_signal(signals_1, signals_2, labels):
-    clipping_value = 1
-    signals_stacked = np.stack((signals_1, signals_2), axis=2)
+def clipping_filter_normalized_signal(signals, labels):
+    clipping_value = 1.0
+    #signals_stacked = np.stack((signals_1, signals_2), axis=2)
 
-    rows_to_delete = np.where(np.where(np.any(signals_stacked == clipping_value, axis=2)))[0]
-    signals_1_deleted = np.delete(signals_1, rows_to_delete, axis=0)
-    signals_2_deleted = np.delete(signals_2, rows_to_delete, axis=0)
+    rows_to_delete = np.where((np.any(abs(signals) == clipping_value, axis=2)))[0]
+    print('rows_to_delete')
+    print(rows_to_delete)
+    print(np.shape(rows_to_delete))
+
+    signals_deleted = np.delete(signals, rows_to_delete, axis=0)
+    #signals_2_deleted = np.delete(signals_2, rows_to_delete, axis=0)
     labels_deleted = np.delete(labels, rows_to_delete, axis=0)
 
-    return signals_1_deleted, signals_2_deleted, labels_deleted
+    return signals_deleted, labels_deleted
 
 
 
@@ -39,23 +43,24 @@ if __name__ == '__main__':
     
     a = np.array([[0, 0, 1, 1],
                   [0, 0, 5, 0],
-                  [0, 0, 0, 0]]
+                  [0, 0, 1, 0]]
                   )
     
     d = np.array([[0, 0, 1, 0],
                   [0, 0, 0, 0],
-                  [0, 0, 0, 0]]
+                  [1, 0, 0, 0]]
                   )
     
-    labels = np.array([0, 0, 1, 0])
+    labels = np.array([0, 1, 0])
 
-    stack = np.stack((a, d), axis=0)
-    print(stack)
-    print(np.shape(a))
+    stack = np.stack((a, d), axis=2)
+    #print(stack)
+    #print(np.shape(a))
     print(np.shape(stack))
+    print(stack)
   
     rows = np.where(np.any(abs(stack) == 1, axis=0))[0]
-    print(rows)
+    #print(rows)
 
     #deleted = np.delete(stack, rows, axis=0)
     #print(deleted)
@@ -66,15 +71,16 @@ if __name__ == '__main__':
     #print(split_2)
 
 
-    a_deleted = np.delete(a, rows, axis=0)
-    d_deleted = np.delete(d, rows, axis=0)
+    #a_deleted = np.delete(a, rows, axis=0)
+    #d_deleted = np.delete(d, rows, axis=0)
 
-    print(a_deleted)
-    print(d_deleted)
+    #print(a_deleted)
+    #print(d_deleted)
 
 
-    a, d, labels =clipping_filter_normalized_signal(a, d, labels)
+    stack, labels =clipping_filter_normalized_signal(stack, labels)
 
-    print(a)
-    print(d)
+    print(stack)
     print(labels)
+    print(np.shape(stack))
+    print(np.shape(labels))
