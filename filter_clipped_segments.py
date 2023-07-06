cb6182ee37c8162afa8448eb56d020bed5b4e13e
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2023-07-06 09:02:25 +0200

    clean function names

--- filter_clipped_segments.py
+++ filter_clipped_segments.py
@@ -6,36 +6,9 @@ import wfdb
 
 from sklearn.model_selection import train_test_split
 
-#from load_dataset_without_filter import load_dataset_PAF
-
-#def clipping_filter(signals):
-
- #   clipping_value = np.bincount(signals).argmax()
- #   
- #   rows_to_delete = np.where(np.where(np.any(signals == clipping_value, axis=2)))[0]
- #   signals_fitered = np.delete(signals, rows_to_delete, axis=0)
- #   
- #   return signals_fitered
 
 
 def clipping_filter_normalized_signal(signals, labels):
-    clipping_value = 1.0
-    #signals_stacked = np.stack((signals_1, signals_2), axis=2)
-
-    rows_to_delete = np.where((np.any(abs(signals) == clipping_value, axis=2)))[0]
-    print('rows_to_delete')
-    print(rows_to_delete)
-    print(np.shape(rows_to_delete))
-
-    signals_deleted = np.delete(signals, rows_to_delete, axis=0)
-    #signals_2_deleted = np.delete(signals_2, rows_to_delete, axis=0)
-    labels_deleted = np.delete(labels, rows_to_delete, axis=0)
-
-    return signals_deleted, labels_deleted
-
-
-
-def clipping_filter_normalized_signal_sliding_window(signals, labels):
     CLIPPING_WINDOW = np.array([1.0, 1.0, 1.0, 1.0, 1.0])
     
     window_view = np.lib.stride_tricks.sliding_window_view(signals, window_shape=np.shape(CLIPPING_WINDOW)[0], axis=1)
@@ -45,8 +18,6 @@ def clipping_filter_normalized_signal_sliding_window(signals, labels):
     labels_deleted = np.delete(labels, rows_to_delete, axis=0)
 
     return signals_deleted, labels_deleted
-
-
 
 
 
