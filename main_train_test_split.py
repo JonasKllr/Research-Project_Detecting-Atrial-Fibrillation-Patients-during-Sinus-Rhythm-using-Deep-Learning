5247736f002d4b18199a17938b2a400fca61f6ff
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2023-07-23 12:42:44 +0200

    rename

--- main_train_test_split.py
+++ main_train_test_split.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
-import tensorboard
+import matplotlib.pyplot as plt
+import os
 
 from datetime import datetime
 from sklearn.model_selection import KFold, train_test_split
@@ -45,10 +46,6 @@ print(np.shape(signals))
 print(np.shape(labels))
 
 
-# set up k-fold cross validation 
-#kfold = KFold(n_splits=5, shuffle=True, random_state=42)
-
-
 
 # split data into train and test sets randomly
 train_data_1, test_data_1, train_data_2, test_data_2, train_labels, test_labels = train_test_split(
@@ -60,19 +57,62 @@ train_data_1, test_data_1, train_data_2, test_data_2, train_labels, test_labels 
 train_data = tf.data.Dataset.from_tensor_slices(((train_data_1, train_data_2), train_labels))
 test_data = tf.data.Dataset.from_tensor_slices(((test_data_1, test_data_2), test_labels))
 
-train_data = train_data.batch(100)
-test_data = test_data.batch(1)
+train_data = train_data.batch(32)
+test_data = test_data.batch(100)
 
-model = my_model.build_model_without_tuner_2()
+model = my_model.build_model_without_tuner_4()
+print(model.summary())
 
 # Tensorboard callback
 log_dir = r'/media/jonas/SSD_new/CMS/Semester_4/research_project/tensorboard/' + datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=2)
 
-model.fit(train_data,
-          epochs = 2,
+#Early Stopping
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=8, restore_best_weights=True)
+
+history = model.fit(train_data,
+          epochs = 30,
           validation_data = test_data,
-          callbacks=tensorboard_callback
+          callbacks=[tensorboard_callback, early_stopping]
           )
+
+# save history plots
+HISTORY_DIR = '/media/jonas/SSD_new/CMS/Semester_4/research_project/history/' + datetime.now().strftime("%Y%m%d-%H%M%S") +'/'
+os.mkdir(HISTORY_DIR)
+
+# Get the training accuracy and validation accuracy
+training_accuracy = history.history['binary_accuracy']
+validation_accuracy = history.history['val_binary_accuracy']
+
+# Convert accuracy to percentage values
+training_accuracy_percent = [acc * 100 for acc in training_accuracy]
+validation_accuracy_percent = [acc * 100 for acc in validation_accuracy]
+
+num_epochs = len(history.history['loss'])
+epochs = range(1, num_epochs + 1)
+
+plt.plot(epochs, training_accuracy_percent)
+plt.plot(epochs, validation_accuracy_percent)
+plt.xticks(epochs)
+plt.ylim(0)
+plt.ylabel('Accuracy [%]')
+plt.xlabel('Epoch [-]')
+plt.legend(['train', 'val'], loc='upper left')
+plt.grid(True)
+#plt.show()
+plt.savefig(HISTORY_DIR + 'acc.png')
+
+plt.clf()
+
+plt.plot(epochs, history.history['loss'])
+plt.plot(epochs, history.history['val_loss'])
+plt.xticks(epochs)
+plt.ylim(0)
+plt.ylabel('Loss [-]')
+plt.xlabel('Epoch [-]')
+plt.legend(['train', 'val'], loc='upper left')
+plt.grid(True)
+#plt.show()
+plt.savefig(HISTORY_DIR + 'loss.png')
 
 del model
