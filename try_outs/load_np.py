a1f012cdbc4682c4d41b6292790efcc6bf2930d8
Jonas Keller <jonas.keller1@mailbox.tu-dresden.de>
2023-08-09 10:03:45 +0200

    changes

--- try_outs/load_np.py
+++ try_outs/load_np.py
@@ -1,13 +1,31 @@
+import sys
+sys.path.append("/media/jonas/SSD_new/CMS/Semester_4/research_project/scripts")
+
 import matplotlib.pyplot as plt
 import numpy as np
+
+from data_pipeline.normalize import normalize_ecg
+from data_pipeline.filter_butterworth import butter_bandpass_filter
 
 SAVE_DIR = '/media/jonas/SSD_new/CMS/Semester_4/research_project/datasets/CinC/training_prepared/'
 
 signals = np.load(SAVE_DIR + 'CinC_signals.npy', allow_pickle=False)
-labels = np.load(SAVE_DIR + 'CinC_labels.npy', allow_pickle=False)
+labels = np.load(SAVE_DIR + 'CinC_labels_5.npy', allow_pickle=False)
 
 print(labels)
 
+
+
+# Butterworth filter
+LOWCUT = 0.3
+HIGHCUT = 50
+FREQUENCY_HERTZ = 128
+ORDER = 5
+
+signals[:,:,0] = butter_bandpass_filter(signals[:,:,0], LOWCUT, HIGHCUT, FREQUENCY_HERTZ, ORDER)
+signals[:,:,1] = butter_bandpass_filter(signals[:,:,1], LOWCUT, HIGHCUT, FREQUENCY_HERTZ, ORDER)
+
+signals = normalize_ecg(signals)
 
 # set record parameters
 RECORD_DURATION_SECONDS = 10
@@ -22,12 +40,14 @@ t = np.linspace(0, RECORD_DURATION_SECONDS, nsamples, endpoint=False)
 fig, axs = plt.subplots(2)
 
 # plot normalized signals
-z = signals[0, :, 0]
+signal_number = 500
+
+z = signals[signal_number, :, 0]
 axs[0].plot(t, z)
 axs[0].set_title('lead 0 normalized', loc='left')
 axs[0].set_xlabel('seconds')
 
-w = signals[0, :, 1]
+w = signals[signal_number, :, 1]
 axs[1].plot(t, w)
 axs[1].set_title('lead 1 normalized', loc='left')
 axs[1].set_xlabel('seconds')
